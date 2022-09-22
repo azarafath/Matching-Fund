708efae9cf34b5ad48d10d60f745ed3878ace0eb
Dimas Fauzan Nurhidayat <dimaz.fauzan85@gmail.com>
2022-09-22 20:06:21 +0700

    slicing ui login, register, dan home page

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Color kBlackColor = const Color(0xff1F1449);
+const defaultRadius = 22.0;
+
+Color kBlackColor = const Color(0xff1A1E1F);
 Color kWhiteColor = const Color(0xffFFFFFF);
 Color kBackgroundColor = const Color(0xffF6F6F6);
 Color kGreenColor = const Color(0xff50C2C9);
 Color kRedColor = const Color(0xffF91919);
+Color kGreyColor = const Color(0xffBDBDBD);
 
 TextStyle blackTextStyle = GoogleFonts.poppins(
   color: kBlackColor,
@@ -15,6 +18,9 @@ TextStyle whiteTextStyle = GoogleFonts.poppins(
 );
 TextStyle greenTektStyle = GoogleFonts.poppins(
   color: kGreenColor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
 );
 
 FontWeight light = FontWeight.w300;
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,1 +1,4 @@
 export 'onboarding_page.dart';
+export 'login_page.dart';
+export 'register_page.dart';
+export 'home_page.dart';
